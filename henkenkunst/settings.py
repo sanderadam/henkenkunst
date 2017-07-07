ef13bbf9e6f17b8c54860cd54bcc1d94daad7668
Sander Adam <mail@ajadam.nl>
2017-07-07 20:59:13 +0200

    forgot debug

--- henkenkunst/settings.py
+++ henkenkunst/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1f(8&#3*--zlvu8e9miwolnqts3vypu&9wbg2r!dr5wt7t*p(*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sanderadam.pythonanywhere.com', '127.0.0.1']
 
